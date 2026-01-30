0f1064293e4ca130bd2bd1d1331e0ee5ce235e9d
vvn-sequeira <svivianmarcel@gmail.com>
2026-01-30 18:34:09 +0530

    Refactor authentication to use axios for API calls in Login and Register components; add CORS configuration in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,14 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000"
+  ],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
 import { authAPI } from '../services/api'
+import axios from 'axios'
 
 const Login = () => {
   // const navigate = useNavigate()
@@ -32,7 +33,13 @@ const Login = () => {
     }
 
     try {
-      const response = await authAPI.login(formData)
+      console.log("triggerd")
+      const response = await axios.post(
+         "http://localhost:5000/api/users/Login",
+         formData ,
+         {withCredentials:true}
+        );
+
       console.log('Login successful:', response)
       // Redirect to profile after successful login
       // navigate('/profile')
