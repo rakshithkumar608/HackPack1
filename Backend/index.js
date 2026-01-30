db532f72561a3ec9e645cc1d4e517a65842d4592
Rakshitih <rakshithkumab01718@gmail.com>
2026-01-30 15:58:26 +0530

    Refactor Login component to improve form handling and enhance user experience with better validation and error management

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoutes = require('./Routes/UserRoutes');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
