e4613da9469c2e3c2bd24a53c56974a23ee36ad0
vvn-sequeira <svivianmarcel@gmail.com>
2026-01-30 16:39:03 +0530

    commit

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // Import controller
 const userController = require('../Controller/UserController');
 
-
 // Login
 router.post('/Login', userController.LoginUser);
 
